1d74dfb4be9d50587f77fc2455cac421784f96fe
EpicFailure <moham3dibr4him@gmail.com>
2025-09-26 19:38:53 +0300

    Add invert filter to the project
    Implemented invert(Image& img) function.
    Integrated filter into main() for testing with sample image.

--- baby_photoshop.cpp
+++ baby_photoshop.cpp
@@ -1,6 +1,29 @@
-#include<bits/stdc++.h>
-#include "Image_class.h"
+#include <bits/stdc++.h>
+#include "Image_Class.h"
 using namespace std;
-int main(){
-  return 0;
+
+void invert(Image& img) {
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
+            for (int k = 0; k < img.channels; k++) {
+                img(i, j, k) = 255 - img(i, j, k);
+            }
+        }
+    }
+}
+
+int main() {
+    string filename;
+    cout << "Enter Image Name With Extension: "<<endl;
+    cin >> filename;
+
+    Image image(filename);
+
+    invert(image);
+    cout<<"Please Enter The Name To Export The Photo W/ Extension: "<<endl;
+    cin >> filename;
+
+    image.saveImage(filename);
+
+    return 0;
 }
