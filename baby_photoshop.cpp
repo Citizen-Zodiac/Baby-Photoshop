e97f97813f8591e1c28b3282884bf9dc602cac1b
EpicFailure <moham3dibr4him@gmail.com>
2025-09-26 22:51:27 +0300

    Blur Filter F(x)
    Adding Blur Filter Function .
    Implementing (5x5 Kernel).
    Function Is Ready To Be Tested.

--- baby_photoshop.cpp
+++ baby_photoshop.cpp
@@ -11,6 +11,32 @@ void invert(Image& img) {
         }
     }
 }
+void blur(Image& img) {
+    Image temp=img;
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
+
+            //Kernel Loops
+            int sum[3]={0,0,0},pixel_cnt=0;
+
+            for (int x = i-2; x <= i+2; x++) {
+                for (int y = j-2; y <= j+2; y++) {
+                    if (x>=0 && x<img.width && y>=0 && y<img.height) {
+                        for (int k = 0; k < 3; k++) {
+                             sum[k]+= temp(x, y, k);
+                        }
+                        pixel_cnt++;
+                    }
+                }
+
+            }
+            // OverWriting The Pixel With Kernel Values
+            for (int k = 0; k < 3; k++) {
+                img(i, j, k) = sum[k]/pixel_cnt;
+            }
+        }
+    }
+}
 
 int main() {
     string filename;
@@ -19,7 +45,7 @@ int main() {
 
     Image image(filename);
 
-    invert(image);
+    blur(image);
     cout<<"Please Enter The Name To Export The Photo W/ Extension: "<<endl;
     cin >> filename;
 
