a1e97f1c12d57ac1f0765d5a42be95d6d7c64ee1
Hamza Mohamed <35264074+HamzaMohamed25@users.noreply.github.com>
2025-09-27 06:22:57 -0700

    Filter Added
    Add black and white filter

--- baby_photoshop.cpp
+++ baby_photoshop.cpp
@@ -1,55 +1,40 @@
 #include <bits/stdc++.h>
-#include "Image_Class.h"
 using namespace std;
+#include "Image_Class.h"
 
-void invert(Image& img) {
-    for (int i = 0; i < img.width; i++) {
-        for (int j = 0; j < img.height; j++) {
-            for (int k = 0; k < img.channels; k++) {
-                img(i, j, k) = 255 - img(i, j, k);
+
+
+void BlackAndWhite(Image& image, int compar_point = 128)
+{
+    for(int i = 0; i < image.width; ++i)
+    {
+        for(int j = 0; j < image.height; ++j)
+        {
+            unsigned int avg = 0;
+            for(int k = 0; k < 3; ++k)
+            {
+                avg += image(i, j, k);
             }
-        }
-    }
-}
-void blur(Image& img) {
-    Image temp=img;
-    for (int i = 0; i < img.width; i++) {
-        for (int j = 0; j < img.height; j++) {
-
-            //Kernel Loops
-            int sum[3]={0,0,0},pixel_cnt=0;
-
-            for (int x = i-2; x <= i+2; x++) {
-                for (int y = j-2; y <= j+2; y++) {
-                    if (x>=0 && x<img.width && y>=0 && y<img.height) {
-                        for (int k = 0; k < 3; k++) {
-                             sum[k]+= temp(x, y, k);
-                        }
-                        pixel_cnt++;
-                    }
-                }
-
+            avg /= 3;
+            unsigned int new_pix = (avg >= compar_point) ? 255 : 0;
+            for (int k = 0; k < 3; ++k)
+            {
+                image.setPixel(i, j, k, new_pix);
             }
-            // OverWriting The Pixel With Kernel Values
-            for (int k = 0; k < 3; k++) {
-                img(i, j, k) = sum[k]/pixel_cnt;
-            }
+            
         }
     }
 }
 
-int main() {
-    string filename;
-    cout << "Enter Image Name With Extension: "<<endl;
-    cin >> filename;
-
-    Image image(filename);
-
-    blur(image);
-    cout<<"Please Enter The Name To Export The Photo W/ Extension: "<<endl;
-    cin >> filename;
-
-    image.saveImage(filename);
-
-    return 0;
+int main()
+{
+    string fileName;
+    cout << "Please enter image filename to open: ";
+    cin >> fileName;
+    
+    Image image(fileName);
+    
+    cout << "Enter output filename (with extension .jpg, .bmp, .png, .tga): ";
+    cin >> fileName;
+    image.saveImage(fileName);
 }
