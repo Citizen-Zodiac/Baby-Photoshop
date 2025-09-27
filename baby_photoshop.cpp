0b44c9128b7f335998d8c0440528f49272b9f995
EpicFailure <moham3dibr4him@gmail.com>
2025-09-27 17:27:09 +0300

    Blur Filter Update.
    - Added input validation for blur intensity (7 As Max).
    - Simplified summation by removing inner loop over channels.
    - Kept single-pass box blur implementation.

--- baby_photoshop.cpp
+++ baby_photoshop.cpp
@@ -13,27 +13,34 @@ void invert(Image& img) {
 }
 void blur(Image& img) {
     Image temp=img;
+    cout<<"Please Enter Blur Intensity Index From 1 : 7"<<endl;
+    int index;
+    cin >> index; if(index>7)index=7;
+    index++;
     for (int i = 0; i < img.width; i++) {
         for (int j = 0; j < img.height; j++) {
 
             //Kernel Loops
             int sum[3]={0,0,0},pixel_cnt=0;
 
-            for (int x = i-2; x <= i+2; x++) {
-                for (int y = j-2; y <= j+2; y++) {
+            for (int x = i-index; x <= i+index; x++) {
+                for (int y = j-index; y <= j+index; y++) {
                     if (x>=0 && x<img.width && y>=0 && y<img.height) {
-                        for (int k = 0; k < 3; k++) {
-                             sum[k]+= temp(x, y, k);
-                        }
+                        sum[0] += temp(x,y,0);
+                        sum[1] += temp(x,y,1);
+                        sum[2] += temp(x,y,2);
+
                         pixel_cnt++;
                     }
                 }
 
             }
             // OverWriting The Pixel With Kernel Values
-            for (int k = 0; k < 3; k++) {
-                img(i, j, k) = sum[k]/pixel_cnt;
-            }
+
+                img(i, j, 0) = sum[0]/pixel_cnt;
+                img(i, j, 1) = sum[1]/pixel_cnt;
+                img(i, j, 2) = sum[2]/pixel_cnt;
+
         }
     }
 }
