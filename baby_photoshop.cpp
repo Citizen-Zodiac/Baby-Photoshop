0d7914f3bca749f01f74016e1e9991f99d147f24
Hamza Mohamed <35264074+HamzaMohamed25@users.noreply.github.com>
2025-09-27 06:31:50 -0700

    Update baby_photoshop.cpp

--- baby_photoshop.cpp
+++ baby_photoshop.cpp
@@ -1,8 +1,42 @@
 #include <bits/stdc++.h>
-using namespace std;
 #include "Image_Class.h"
+using namespace std;
 
+void invert(Image& img) {
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
+            for (int k = 0; k < img.channels; k++) {
+                img(i, j, k) = 255 - img(i, j, k);
+            }
+        }
+    }
+}
+void blur(Image& img) {
+    Image temp=img;
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
 
+            //Kernel Loops
+            int sum[3]={0,0,0},pixel_cnt=0;
+
+            for (int x = i-2; x <= i+2; x++) {
+                for (int y = j-2; y <= j+2; y++) {
+                    if (x>=0 && x<img.width && y>=0 && y<img.height) {
+                        for (int k = 0; k < 3; k++) {
+                             sum[k]+= temp(x, y, k);
+                        }
+                        pixel_cnt++;
+                    }
+                }
+
+            }
+            // OverWriting The Pixel With Kernel Values
+            for (int k = 0; k < 3; k++) {
+                img(i, j, k) = sum[k]/pixel_cnt;
+            }
+        }
+    }
+}
 
 void BlackAndWhite(Image& image, int compar_point = 128)
 {
@@ -26,15 +60,18 @@ void BlackAndWhite(Image& image, int compar_point = 128)
     }
 }
 
-int main()
-{
-    string fileName;
-    cout << "Please enter image filename to open: ";
-    cin >> fileName;
-    
-    Image image(fileName);
-    
-    cout << "Enter output filename (with extension .jpg, .bmp, .png, .tga): ";
-    cin >> fileName;
-    image.saveImage(fileName);
+int main() {
+    string filename;
+    cout << "Enter Image Name With Extension: "<<endl;
+    cin >> filename;
+
+    Image image(filename);
+
+    blur(image);
+    cout<<"Please Enter The Name To Export The Photo W/ Extension: "<<endl;
+    cin >> filename;
+
+    image.saveImage(filename);
+
+    return 0;
 }
