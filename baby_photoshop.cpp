fe0f9eeeae40a8de0bcee4d0a2c503f7cc931b80
Hamza Mohamed <35264074+HamzaMohamed25@users.noreply.github.com>
2025-09-27 06:35:17 -0700

    Update baby_photoshop.cpp

--- baby_photoshop.cpp
+++ baby_photoshop.cpp
@@ -38,6 +38,31 @@ void blur(Image& img) {
     }
 }
 
+
+void BlackAndWhite(Image& image, int compar_point = 128)
+{
+    for(int i = 0; i < image.width; ++i)
+    {
+        for(int j = 0; j < image.height; ++j)
+        {
+            unsigned int avg = 0;
+            for(int k = 0; k < 3; ++k)
+            {
+                avg += image(i, j, k);
+            }
+            avg /= 3;
+            unsigned int new_pix = (avg >= compar_point) ? 255 : 0;
+            for (int k = 0; k < 3; ++k)
+            {
+                image.setPixel(i, j, k, new_pix);
+            }
+            
+        }
+    }
+
+}
+
+
 int main() {
     string filename;
     cout << "Enter Image Name With Extension: "<<endl;
